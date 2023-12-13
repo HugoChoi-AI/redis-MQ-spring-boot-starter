2ef23273a396c2160e80b903dd39f245b6ceccc6
HugoC <jianxiong.cai@qima.com>
2023-12-13 15:24:26 +0800

    feat: customize exception

--- src/main/java/com/qima/redismq/exception/FailedHandleMessageException.java
+++ src/main/java/com/qima/redismq/exception/FailedHandleMessageException.java
@@ -5,4 +5,12 @@ public class FailedHandleMessageException extends Throwable{
     public FailedHandleMessageException(String message) {
         super(message);
     }
+
+    public FailedHandleMessageException(Throwable cause) {
+        super(cause);
+    }
+
+    public FailedHandleMessageException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
